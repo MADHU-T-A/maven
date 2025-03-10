23329e9fe9a4ac48bae742be3da1850492a49aa8
MADHU T <MADHU T@LAPTOP-NUAUSDAK>
2025-03-10 19:34:53 +0530

    first commit maven

---
+++ MAVEN/src/test/java/com/luftree/testscript/customer.java
@@ -0,0 +1,19 @@
+package com.luftree.testscript;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class customer {
+
+
+    @Test
+    public void testReporterLog() {
+        // Set up the WebDriver (in this case, ChromeDriver)
+        //System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        //WebDriver driver = new ChromeDriver();
+        Reporter.log("create customer",true);
+System.out.println("hello");
+	
+}
+}
